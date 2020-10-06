eb7daa5e9eac73510c43cf6564c16bf67e8e3b5a
WangZhengtao1993 <wangzhengtao@sjtu.edu.cn>
2020-10-06 17:04:42 +0800

    excel绘图

--- code/main.py
+++ code/main.py
@@ -3,8 +3,15 @@ import pandas as pd
 import os
 import win32com.client as win32
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
 from numpy import *
 import openpyxl
+from openpyxl.chart import (
+    Reference,
+    series,
+    BarChart,
+    LineChart
+)
 
 
 # win32api找不到时 pip install pywin32==225
@@ -12,14 +19,16 @@ import openpyxl
 class EMGProcess(object):
 
     def __init__(self, folder, subject):
+        self.upper_limb = False
         self.header = 4  # 表头在第四行
         self.folder = folder  # 文件夹路径
-        if os.path.exists:
+        # 新建文件夹
+        if os.path.exists(self.folder + "\\Process\\"):
             pass
         else:
             os.mkdir(self.folder + "\\Process\\")
         self.process_folder = self.folder + "\\Process\\"
-        self.win_set = (100, 100)
+        self.win_set = (100, 100)  # 滑动时间窗
         self.subject = subject
         print("Path:", folder)
 
@@ -31,13 +40,20 @@ class EMGProcess(object):
         print(file_name)
 
         # 遍历所有文件
+        if self.upper_limb:
+            motion_id_start = 0
+            motion_id_end = 6
+        else:
+            motion_id_start = 7
+            motion_id_end = 13
+
         keyword = "Odau"
         for file in file_list:
             if keyword in file:
                 print("file", file[26:29])
                 file_id = file[26:29]
                 # 遍历所有文件id
-                for motion in range(0, 6):
+                for motion in range(motion_id_start, motion_id_end):
                     for motion_pattern in range(1, 4):
                         # print("motion:", motion)
                         # print("motion_pattern:", motion_pattern)
@@ -56,7 +72,6 @@ class EMGProcess(object):
         # src_file_name = f[26:29]
 
     def save_as_high_ver(self):
-
         """
         源文件格式错误，另存为xlsx
         """
@@ -68,13 +83,14 @@ class EMGProcess(object):
             file_list = os.listdir(self.folder)
             # 2.打开excel处理程序，固定写法
             excel = win32.gencache.EnsureDispatch('Excel.Application')
-
+            # excel.Application.Visible = False
             print("saving as .xlsx")
             for file in file_list:
                 # 将文件名与后缀分开
                 file_name, suff = os.path.splitext(file)
                 if suff == ".xls":
                     wb = excel.Workbooks.Open(self.folder + "\\" + file)
+                    # wb.Application.Visible = False
                     wb.SaveAs(self.process_folder + file + "x", FileFormat=51)
                     # FileFormat = 51 is for .xlsx extension
                     # FileFormat = 56 is for .xls extension
@@ -99,7 +115,7 @@ class EMGProcess(object):
                     print("新建肌电信号列")
                     data["Frame"] = data["Frame"] / 1000
                     data.rename(columns={"Frame": "time"}, inplace=True)
-                    print("Data:", data)
+                    # print("Data:", data)
                     for i in range(1, 7):
                         EMG = ("EMG_" + str(i))
                         analog = ("Analog_" + str(i))
@@ -129,14 +145,22 @@ class EMGProcess(object):
                                            "RMS_5": rms_data['RMS_5'],
                                            "RMS_6": rms_data['RMS_6'],
                                            })
-
-                    df_rms.rename(columns={"RMS_1": "RMS_1_三角肌前束",
-                                           "RMS_2": "RMS_2_三角肌后束",
-                                           "RMS_3": "RMS_3_肱二头肌",
-                                           "RMS_4": "RMS_4_肱三头肌",
-                                           "RMS_5": "RMS_5_桡侧腕屈肌",
-                                           "RMS_6": "RMS_6_尺侧腕伸肌",
-                                           }, inplace=True)
+                    if self.upper_limb:
+                        df_rms.rename(columns={"RMS_1": "RMS_1_三角肌前束",
+                                               "RMS_2": "RMS_2_三角肌后束",
+                                               "RMS_3": "RMS_3_肱二头肌",
+                                               "RMS_4": "RMS_4_肱三头肌",
+                                               "RMS_5": "RMS_5_桡侧腕屈肌",
+                                               "RMS_6": "RMS_6_尺侧腕伸肌",
+                                               }, inplace=True)
+                    else:
+                        df_rms.rename(columns={"RMS_1": "RMS_1_股直肌",
+                                               "RMS_2": "RMS_2_股二头肌",
+                                               "RMS_3": "RMS_3_半腱肌",
+                                               "RMS_4": "RMS_4_股内侧肌",
+                                               "RMS_5": "RMS_5_胫骨前肌",
+                                               "RMS_6": "RMS_6_外侧腓肠肌",
+                                               }, inplace=True)
 
                     # 分别写入两张表
                     writer = pd.ExcelWriter(self.process_folder + file)
@@ -199,6 +223,8 @@ class EMGProcess(object):
     def rms_plot(self):
         keyword = "Odau"
         file_list = os.listdir(self.process_folder)
+
+        font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=15)
         for file in file_list:
             if "plot.txt" in file_list:
                 print("列已添加")
@@ -206,24 +232,76 @@ class EMGProcess(object):
             else:
                 if keyword in file:
                     rms = pd.read_excel(self.process_folder + file, sheet_name="RMS")
-                    rms.plot(x="time", y=["RMS_1", "RMS_2", "RMS_3", "RMS_4", "RMS_5", "RMS_6"])
-                    plt.title("test")
+                    rms.plot(x="time", y=[1, 2, 3, 4, 5, 6])
+                    plt.title(file[-11:-5], font=font)
+                    plt.ylabel('RMS/mV')
                     plt.show()
         print("图已添加")
+
+    def plot_in_excel(self):
+        keyword = "Odau"
+        file_list = os.listdir(self.process_folder)
+        for file in file_list:
+            if keyword in file:
+                wb = openpyxl.load_workbook(self.folder + "\\Process\\" + file)
+                print("open:", self.folder + "\\Process\\" + file)
+                print(wb.sheetnames)
+
+                # ws = wb.get_sheet_by_name("RMS")
+                ws = wb["RMS"]
+                max_row = ws.max_row
+                print("max_row", max_row)
+
+                chart1 = LineChart()
+                # chart1.type = "col"
+                chart1.style = 10
+                chart1.title = "RMS"
+
+                labels = Reference(ws, min_col=2, max_col=7, min_row=1)
+                cats = Reference(ws, min_col=1, min_row=2, max_row=max_row + 1)
+                data1 = Reference(ws, min_col=2, min_row=1, max_row=max_row + 1)
+                data2 = Reference(ws, min_col=3, min_row=1, max_row=max_row + 1)
+                data3 = Reference(ws, min_col=4, min_row=1, max_row=max_row + 1)
+                data4 = Reference(ws, min_col=5, min_row=1, max_row=max_row + 1)
+                data5 = Reference(ws, min_col=6, min_row=1, max_row=max_row + 1)
+                data6 = Reference(ws, min_col=7, min_row=1, max_row=max_row + 1)
+
+                chart1.add_data(data1, titles_from_data=True)
+                chart1.add_data(data2, titles_from_data=True)
+                chart1.add_data(data3, titles_from_data=True)
+                chart1.add_data(data4, titles_from_data=True)
+                chart1.add_data(data5, titles_from_data=True)
+                chart1.add_data(data6, titles_from_data=True)
+
+                chart1.set_categories(cats)
+                # s1 = chart1.series[0:6]
+                # s1.graphicalProperties.line.width = 1000
+                chart1.width = 40
+                chart1.height = 20
+                chart1.x_axis.title = "time/s"
+                chart1.y_axis.title = "RMS/mV"
+                chart1.y_axis.scaling.max = 0.2
+
+
+                ws.add_chart(chart1, "I5")
+                wb.save(self.folder + "\\Process\\" + file)
 
     def run(self):
         # 1.另存为高版本，测试文件重命名对应动作
         self.save_as_high_ver()
-        # self.rename_test_file()
-        # # # 2.肌电信号预处理，矫正零偏，计算RMS
-        # self.new_columns()
+        self.rename_test_file()
+        # # 2.肌电信号预处理，矫正零偏，计算RMS
+        self.new_columns()
         # # # 3. 插入图像
+        self.plot_in_excel()
         # self.rms_plot()
 
 
 def main():
-    folder = r"D:\code\运动能力分析实验\0921wrj_2020_09_21_140406"
+    # folder = r"D:\code\运动能力分析实验\0921wrj_2020_09_21_140406"
+    folder = r"D:\code\运动能力分析实验\0921wrj_2020_09_21_144822"
     subject = r"D:\code\运动能力分析实验\邬如靖.xlsx"
+
     EP = EMGProcess(folder, subject)
     EP.run()
 
--- code/try1006.py
+++ code/try1006.py
@@ -1,5 +1,96 @@
 # author:wzt
 import pandas as pd
+import openpyxl
+
+from openpyxl.chart import(
+    Reference,
+    Series,
+    PieChart,
+    BarChart,
+    BubbleChart,
+    LineChart
+)
+
+# Pie chart
+
+wb = openpyxl.Workbook()
+ws = wb.active
+ws.title = "PieChart"
+
+data = [
+    ["pie", "sold"],
+    ["Apple", 50],
+    ["Cherry", 30],
+    ["Pumpkin", 10],
+    ["Chocolate", 40]
+]
+
+for row in data:
+    ws.append(row)
+
+pie = PieChart()
+labels = Reference(ws, min_col=1, min_row=2)
+data = Reference(ws, min_col=2, min_row=2)
+pie.add_data(data)
+pie.set_categories(labels)
+pie.title = "Pie sold by category"
+ws.add_chart(pie, "A15")
+
+
+ws = wb.create_sheet("barchart")
+rows = [
+    ("Num", "Batch_1", "batch_2"),
+    (1,2,3),
+    (2,3,4),
+    (3,4,5),
+    (4,5,6)
+]
+
+
+for row in rows:
+    ws.append(row)
+chart1 = LineChart()
+chart1.type = "col"
+chart1.style = 15
+chart1.title = "Bar"
+chart1.y_axis.title = "batch"
+cats = Reference(ws, min_col=1, min_row=2, max_row=5)
+data1 = Reference(ws, min_col=2, min_row=2, max_row=5)
+data2 = Reference(ws, min_col=3, min_row=2, max_row=5)
+chart1.add_data(data1)
+chart1.add_data(data2)
+
+chart1.set_categories(cats)
+ws.add_chart(chart1, "B5")
+
+# for row in rows:
+#     ws.append(row)
+# chart1 = BarChart()
+# chart1.type = "col"
+# chart1.style = 15
+# chart1.title = "Bar"
+# chart1.y_axis.title = "batch"
+# cats = Reference(ws, min_col=1, min_row=2, max_row=5)
+# data = Reference(ws, min_col=2, min_row=2, max_row=5)
+# chart1.add_data(data)
+# chart1.set_categories(cats)
+# ws.add_chart(chart1, "B5")
+
+
+
+
+wb.save("charttest.xlsx")
+
+
+
+
+
+
+
+
+
+
+
 
 # books = pd.read_excel("test1.xlsx",skiprows=9, usecols="D:E",dtype={"ID":str})
 # print(books)
@@ -8,27 +99,24 @@ import pandas as pd
 #
 # print(books)
 
-
-f = "0921wrj_2020_09_21_140406_001_Odau_1"
-
-print(f[26:29])
-file_name = pd.read_excel("邬如靖.xlsx", dtype=str)
-print(file_name.columns[2])
-print(file_name)
-
-print(file_name.iloc[1, 3])
-
-src_file_name = f[26:29]
-
-src_file_name_list = []
-for motion in range(0, 6):
-    for motion_pattern in range(2, 5):
-        src_file_name = f[0:26]+file_name.iloc[motion,motion_pattern]+"_Odau_1.xlsx"
-        src_file_name_list.append(src_file_name)
-        new_file_name = f[0:26] + file_name.iloc[motion, motion_pattern] + "_Odau_1" + \
-                                file_name.columns[motion_pattern] + file_name.iloc[motion, 1] + ".xlsx"
-        print(new_file_name)
-print(src_file_name_list)
+# print(f[26:29])
+# file_name = pd.read_excel("邬如靖.xlsx", dtype=str)
+# print(file_name.columns[2])
+# print(file_name)
+#
+# print(file_name.iloc[1, 3])
+#
+# src_file_name = f[26:29]
+#
+# src_file_name_list = []
+# for motion in range(0, 6):
+#     for motion_pattern in range(2, 5):
+#         src_file_name = f[0:26]+file_name.iloc[motion,motion_pattern]+"_Odau_1.xlsx"
+#         src_file_name_list.append(src_file_name)
+#         new_file_name = f[0:26] + file_name.iloc[motion, motion_pattern] + "_Odau_1" + \
+#                                 file_name.columns[motion_pattern] + file_name.iloc[motion, 1] + ".xlsx"
+#         print(new_file_name)
+# print(src_file_name_list)
 
 #
 # for motion in range(1, 7):
@@ -36,3 +124,4 @@ print(src_file_name_list)
 #         new_file_name = f+file_name.columns[type]+file_name.iloc[motion,1]
 #
 # print(new_file_name)
+
